4e01aac75bcc3cf8afe439315bbd89cd1ca7ba1d
stegu <stegu@itn.liu.se>
2011-10-11 15:37:25 +0200

    Bug in 3D classic noise (mod289 vec4 -> vec3)

--- src/classicnoise3D.glsl
+++ src/classicnoise3D.glsl
@@ -17,6 +17,11 @@ vec3 mod289(vec3 x)
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
+vec4 mod289(vec4 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
 vec4 permute(vec4 x)
 {
   return mod289(((x*34.0)+1.0)*x);
