4d1c77acf459a13e5788967ca595db0173a3490e
finscn <fins@163.com>
2017-10-24 09:39:59 +0800

    Cosmetic for code format

--- src/noise2D.glsl
+++ src/noise2D.glsl
@@ -21,8 +21,7 @@ vec3 permute(vec3 x) {
   return mod289(((x*34.0)+1.0)*x);
 }
 
-float snoise(vec2 v)
-  {
+float snoise(vec2 v) {
   const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
                       0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
                      -0.577350269189626,  // -1.0 + 2.0 * C.x
