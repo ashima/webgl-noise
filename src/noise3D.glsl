21d9fe23d75b68807e6692de071efa0818a9a7bb
Stefan Gustavson <stefan.gustavson@gmail.com>
2020-10-14 09:55:56 +0200

    Bug fix
    Fixed an age-old bug that caused slight discontinuities along simplex boundaries.

--- src/noise3D.glsl
+++ src/noise3D.glsl
@@ -96,8 +96,8 @@ float snoise(vec3 v)
   p3 *= norm.w;
 
 // Mix final noise value
-  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  vec4 m = max(0.5 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
   m = m * m;
-  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+  return 106.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
                                 dot(p2,x2), dot(p3,x3) ) );
   }
